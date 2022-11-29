fd642f00c4076f83b4c2e3ccdbd0b8e1d0272a23
FilipHojan <81771162+FilipHojan@users.noreply.github.com>
2022-11-29 23:21:42 +0100

    1 zmiana

--- Biblioteka online/Form1.cs
+++ Biblioteka online/Form1.cs
@@ -16,5 +16,10 @@ namespace Biblioteka_online
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
